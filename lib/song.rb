fdcad95ec7312960b3daf7f5adb8c38eafe06a0c
Micah Shute <micah.shute@gmail.com>
2018-06-28 07:25:41 +0000

    automatically backed up by learn

---
+++ lib/song.rb
@@ -0,0 +1,33 @@
+class Song
+  @@count = 0
+  @@genres = {}
+  @@artists = {}
+
+  attr_accessor :name, :artist, :genre
+
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@count += 1
+    @@genres.keys.has?(genre) ? @@genres[genre] += 1 : @@genres[genre] = 1
+    @@artists.keys.has?(artist) ? @@artists[artist] += 1 : @@artist[artist] = 1
+  end
+
+  def self.genres
+    @@genres.keys
+  end
+
+  def self.artists
+    @@artists.keys
+  end
+
+  def self.genre_count
+    @@genres
+  end
+
+  def self.artist_count
+    @@artists
+  end
+  
+end
